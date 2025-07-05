d5c21fb5012ffdeadaafcae1f47aeaf866cab7c0
Dan Cundy <dancundy@hotmail.com>
2025-07-05 22:43:36 +0100

    Show balance before adding transaction

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -27,6 +27,11 @@ func (b *Bot) Start() {
 		prices := []float64{1.10, 1.12, 1.11, 1.15, 1.13, 1.18, 1.16, 1.20, 1.22, 1.19, 1.23}
 		for _, price := range prices {
 			action, _ := b.strategy.Execute(price)
+			if b.wallet.Balance <= 0 {
+				println("Insufficient balance to continue trading.")
+				b.Stop()
+				return
+			}
 			if action == "buy" {
 				b.wallet.Transactions = w.AddTransaction(b.wallet.Transactions, "withdrawl", price)
 			}
@@ -34,12 +39,8 @@ func (b *Bot) Start() {
 				b.wallet.Transactions = w.AddTransaction(b.wallet.Transactions, "deposit", price)
 			}
 			println("Action:", action)
-			println("Current Balance:", fmt.Sprintf("%.2f", b.wallet.CalculateBalance(b.wallet.Transactions)))
-
-			if b.wallet.Balance <= 0 {
-				println("Insufficient balance to continue trading.")
-				b.Stop()
-				return
+			if action == "sell" || action == "buy" {
+				println("Current Balance:", fmt.Sprintf("%.2f", b.wallet.CalculateBalance(b.wallet.Transactions)))
 			}
 		}
 	}
